e65de617919cf27c27788db36aa544ee212e497d
Sivamurugan <94527948+Siva-SMR@users.noreply.github.com>
2022-03-21 10:44:50 +0530

    Add files via upload

---
+++ Extra/student.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class student {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the name : ");
+        String name = sc.nextLine();
+        System.out.print("Enter the rollno : ");
+        int rollno = sc.nextInt();
+        System.out.print("Enter the mark : ");
+        int mark = sc.nextInt();
+        System.out.println("Name is : "+name);
+        System.out.println("Roll No is : "+rollno);
+        System.out.println("Mark is : "+mark);
+        sc.close();
+
+	}
+
+}
