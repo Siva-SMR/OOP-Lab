2bae37605f0eb172500fd506b879d9740a5bf8ca
Sivamurugan <94527948+Siva-SMR@users.noreply.github.com>
2022-05-23 12:11:08 +0530

    Update pg5.java

--- pg5.java
+++ pg5.java
@@ -1,6 +1,36 @@
 package C3;
 
-public class pg5 {
+import java.util.*;
+
+class Student
+{
+	String name;
+	int id,mark;
+	Student(String name,int id,int mark)
+	{
+		this.name=name;
+		this.id=id;
+		this.mark=mark;
+	}
+}
+
+class Sports
+{
+	String sname;
+	int trophies;
+	Sports(String name,int trophies)
+	{
+		this.sname=sname;
+		this.trophies=trophies;
+	}
+}
+
+class Result extends Student,Sports
+{
+	
+}
+public class pg5
+{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
