5e0251dfda922edf04b795fa8eaef921d158ec4a
Sivamurugan <94527948+Siva-SMR@users.noreply.github.com>
2022-06-06 12:09:18 +0530

    Add files via upload

---
+++ pg1.java
@@ -0,0 +1,37 @@
+package Graphics;
+
+interface figures
+{
+	void rectangle(int length,int breadth);
+	void triangle(int breadth, int height);
+	void square(int a);
+	void circle(int r);
+	
+}
+
+public class pg1 implements figures
+{
+	public void rectangle(int length,int breadth)
+	{
+		System.out.println("Area of the rectangle: "+length* breadth);
+		
+	}
+	
+	public void triangle(int breadth, int height)
+	{
+		System.out.println("Area of the triangle: "+(0.5)*breadth*height);
+		
+	}
+	
+	public void square(int a)
+	{
+		System.out.println("Area of the square: "+a*a);
+		
+	}
+	
+	public void circle(int r)
+	{
+		System.out.println("Area of the circle: "+(3.14)*r*r);
+		
+	}
+}
